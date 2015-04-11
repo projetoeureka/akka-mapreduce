0bbc9cf5638b7c46dc93f9f7bdc340823cd10242
nlw0 <nicolau@geekie.com.br>
2015-04-11 20:47:19 -0300

    improving the low-level file chunk line reader, using some fluent style, etc

--- src/main/java/FileCounterIterator.java
+++ src/main/java/FileCounterIterator.java
@@ -4,6 +4,9 @@ import java.io.*;
 
 /**
  * Created by nlw on 11/04/15.
+ * This class reads lines from a buffered file iterator, and also provides a
+ * byte count of the data read. This lets you to know the file position you
+ * are reading from.
  */
 public class FileCounterIterator {
 
@@ -15,30 +18,37 @@ public class FileCounterIterator {
         return _fileSize;
     }
 
+    public FileCounterIterator newlineLength(Long newNewlineLength) {
+        this._newlineLength = newNewlineLength;
+        return this;
+    }
+
     private Long _fileSize = 0L;
     private Long _position = 0L;
+    private Long _newlineLength = 1L;
     private RandomAccessFile fp;
     private BufferedReader itr;
 
     public FileCounterIterator(String filename) throws IOException {
         fp = new RandomAccessFile(filename, "r");
-        itr = new BufferedReader(new InputStreamReader(new FileInputStream(fp.getFD())));
         _fileSize = fp.length();
+        this.seek(0L);
+    }
+
+    public FileCounterIterator seek(Long newPosition) throws IOException {
+        this.fp.seek(newPosition);
+        this._position = newPosition;
+        itr = new BufferedReader(new InputStreamReader(new FileInputStream(fp.getFD())));
+        return this;
     }
 
     public Boolean hasNext() throws IOException {
         return this._position < this._fileSize;
     }
 
-    public void seek(Long newPosition) throws IOException {
-        this.fp.seek(newPosition);
-        this._position = newPosition;
-    }
-
     public String readLine() throws IOException {
-        String nextLine = "";
-        nextLine = itr.readLine();
-        this._position += nextLine.getBytes().length + 1;
+        String nextLine = itr.readLine();
+        this._position += nextLine.getBytes().length + _newlineLength;
         return nextLine;
     }
 }
--- src/main/scala/geekie/mapred/io/FileChunkLineReader.scala
+++ src/main/scala/geekie/mapred/io/FileChunkLineReader.scala
@@ -1,64 +1,38 @@
-/**
- * Created by nlw on 06/04/15. Line-oriented file chunk reading utilities.
- *
- */
 package geekie.mapred.io
 
-/*
-import java.io._
-import java.nio.charset.StandardCharsets
-*/
-
-
+/**
+ * Created by nlw on 06/04/15.
+ * Line-oriented file chunk reading utilities. Useful for map-reduce tasks on large files.
+ *
+ */
 
 class FileChunkLineReader(filename: String, start: Int, end: Int) extends Iterable[String] {
-  println(f"file chunk $start $end")
-
   def iterator = new Iterator[String] {
-    val lineItr = new FileCounterIterator(filename)
+    private val lineItr = new FileCounterIterator(filename)
 
-    if (start > 0) {
-      lineItr.seek(start)
-      lineItr.readLine()
-    }
+    if (start > 0) lineItr.seek(start).readLine()
 
-    def hasNext = lineItr.position < end && lineItr.hasNext
+    def hasNext = lineItr.position <= end && lineItr.hasNext
 
     def next() = lineItr.readLine()
   }
 }
 
 object FileChunkLineReader {
+  def apply(filename: String, start: Int, end: Int) = new FileChunkLineReader(filename, start, end)
   def apply(filename: String)(range: (Int, Int)) = new FileChunkLineReader(filename, range._1, range._2)
 }
 
-/*
-class SmartFileIterator(filename: String) {
-  val fp = new RandomAccessFile(filename, "r")
-  val itr = new BufferedReader(new InputStreamReader(new FileInputStream(fp.getFD)))
-  val fileSize = fp.length
-  private var _position: Long = 0
-
-  def position = _position
-
-  def hasNext = position < fileSize
-
-  def seek(newPosition: Long) = {
-    fp.seek(newPosition)
-    _position = newPosition
-  }
-
-  def readLine() = {
-    val nextLine = itr.readLine()
-    _position += nextLine.getBytes.length + 1
-    nextLine
-  }
-}
-*/
-
 object ChunkLimits {
   def apply(end: Int, nChunks: Int) = {
-    val skip = (end + nChunks) / nChunks
+    val skip = (end + 1) / nChunks
+    ((0 until end by skip) zip (skip until end by skip)) :+((end - 1) / skip * skip, end)
+  }
+}
+
+object BresenhamChunkLimits { //Bresenham!
+  def apply(end: Int, nChunks: Int) = {
+    val skip = (end + 1) / nChunks
     ((0 until end by skip) zip (skip until end by skip)) :+((end - 1) / skip * skip, end)
   }
 }
